f3dc2e73d0b09f784f5bea3c5bb9e538dc850c0f
Gunshot Sound Studios <zeindigorenderer@gmail.com>
2024-09-03 15:29:04 +0100

    Fixed, `color`, added `clear` and `resize`
    Co-authored-by: msix29 <msix29@users.noreply.github.com>

--- tests/init.client.lua
+++ tests/init.client.lua
@@ -11,6 +11,25 @@ local SIZE_Y = 1024
 
 local OSGL = require(ReplicatedStorage.OSGL)
 local Window = OSGL.Window
-local Draw = OSGL.Draw
-local Color = OSGL.Color
+local draw = OSGL.draw
+local color = OSGL.color
 
+local myWindow = Window.new(windowUi, { sizeX = 8, sizeY = 4 })
+myWindow:Clear(color.BLUE)
+myWindow:Resize(4, 4)
+myWindow:Render() 
+draw.pixel(myWindow, 0, 0, color.RED)
+myWindow:Render()
+myWindow:Resize(8, 4) --// Changes width to 100
+myWindow:Render()
+
+-- Runs every heartbeat
+-- while myWindow:IsOpen() do
+--     -- Render here
+
+--     myWindow:Clear(color.WHITE)
+--     draw.pixel(myWindow, 49, 24, color.BLUE)
+--     -- Stop rendering here
+
+--     myWindow:Render()
+-- end
