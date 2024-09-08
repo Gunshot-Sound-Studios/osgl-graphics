9a12ab777262851ccdba6795ded7e8147482a8d6
Gunshot Sound Studios <zeindigorenderer@gmail.com>
2024-09-08 08:11:07 +0100

    Added `GetRelativeMouse` method

--- tests/init.client.lua
+++ tests/init.client.lua
@@ -3,6 +3,7 @@
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
+local UserInputService = game:GetService("UserInputService")
 
 local windowUi = Players.LocalPlayer.PlayerGui:WaitForChild("ScreenGui").Window
 
@@ -18,31 +19,18 @@ local color = OSGL.color
 
 local myWindow = Window.new(windowUi, { sizeX = 50, sizeY = 50 })
 
-local emptyTexture = texture.new(185, 436)
-
--- local txt = texture.from(script.sprite)
--- local mySprite = sprite.new(txt)
--- local secondSprite = sprite.new(emptyTexture)
--- -- mySprite:Draw(emptyTexture)
-
-myWindow:Clear(color.MAGENTA)
-draw.rectangle(myWindow, {
-	xPos = 0,
-	yPos = 0,
-	width = 25,
-	height = 25,
-	fillColor = color.RED
-})
-
-print(texture.read(myWindow, 0, 0,  5, 5))
-print(texture.read(myWindow, 30, 30,  5, 5))
 
 -- Runs every heartbeat
-while myWindow:IsOpen() do
-	
-	-- secondSprite:Draw(myWindow)
-	myWindow:Render()
 
+myWindow:Clear(color.BLACK)
+while myWindow:IsOpen() do
+	local x, y = myWindow:GetRelativeMouse()
+	
+	if x > 0 and y > 0 and UserInputService:IsMouseButtonPressed(Enum.UserInputType.MouseButton1) then
+		draw.pixel(myWindow, x, y, color.RED)
+	end
+
+	myWindow:Render()
 end
 
 print("The window has been destroyed.")
