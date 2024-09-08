726e56f18f8beb2b48d90c6212ab3b1f355b8bb3
Gunshot Sound Studios <zeindigorenderer@gmail.com>
2024-09-08 07:50:38 +0100

    Fixed `MAGENTA` color and added `texture.read`

--- tests/init.client.lua
+++ tests/init.client.lua
@@ -16,27 +16,31 @@ local sprite = OSGL.Sprite
 local draw = OSGL.draw
 local color = OSGL.color
 
-local myWindow = Window.new(windowUi, { sizeX = 185, sizeY = 436 })
+local myWindow = Window.new(windowUi, { sizeX = 50, sizeY = 50 })
 
 local emptyTexture = texture.new(185, 436)
 
-local txt = texture.from(script.sprite)
-local mySprite = sprite.new(txt)
-local secondSprite = sprite.new(emptyTexture)
-mySprite:Draw(emptyTexture)
+-- local txt = texture.from(script.sprite)
+-- local mySprite = sprite.new(txt)
+-- local secondSprite = sprite.new(emptyTexture)
+-- -- mySprite:Draw(emptyTexture)
 
-draw.rectangle(emptyTexture, {
+myWindow:Clear(color.MAGENTA)
+draw.rectangle(myWindow, {
 	xPos = 0,
 	yPos = 0,
-	width = 93,
-	height = 218,
+	width = 25,
+	height = 25,
 	fillColor = color.RED
 })
+
+print(texture.read(myWindow, 0, 0,  5, 5))
+print(texture.read(myWindow, 30, 30,  5, 5))
 
 -- Runs every heartbeat
 while myWindow:IsOpen() do
 	
-	secondSprite:Draw(myWindow)
+	-- secondSprite:Draw(myWindow)
 	myWindow:Render()
 
 end
