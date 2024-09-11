d1783585cb45c092fcd1a05fd192efe40e137ea7
msix29 <abdulrehmanessam7567@gmail.com>
2024-09-11 23:57:52 +0300

    Add `image_to_lua` module (function)

---
+++ examples/image-editor/src/image_to_lua.lua
@@ -0,0 +1,35 @@
+local types = require(script.Parent.OSGL.types)
+
+return function(width: number, height: number, pixels: { types.Color }): string
+    local last_color = pixels[1]
+    local repetitions = 0
+    local module_text = "return {\n\t"
+        .. `width = \{{width}},\n\t`
+        .. `height = \{{height}},\n\t`
+        .. "pixels = {"
+
+    for _, color in ipairs(pixels) do
+        if color == last_color then
+            repetitions += 1
+
+            continue
+        end
+
+        if repetitions > 1 then
+            module_text ..= `"r{repetitions}-{last_color}",`
+            repetitions = 0
+        elseif repetitions == 0 then
+            module_text ..= tostring(last_color)
+            repetitions = 0
+        end
+
+        last_color = color
+        repetitions += 1
+    end
+
+    if repetitions ~= 0 then
+        module_text ..= `"r{repetitions}-{last_color}"`
+    end
+
+    return `{module_text}},\n}`
+end
