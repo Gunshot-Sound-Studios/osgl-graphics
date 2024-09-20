61a941d4427d9b7c49346bc47c5b6a5c42c62f12
msix29 <abdulrehmanessam7567@gmail.com>
2024-09-20 10:39:51 +0300

    Update testing code

--- tests/init.client.lua
+++ tests/init.client.lua
@@ -9,7 +9,30 @@ local windowUi = Players.LocalPlayer.PlayerGui:WaitForChild("ScreenGui").Window
 local OSGL = require(ReplicatedStorage.OSGL)
 local Window = OSGL.Window
 local color = OSGL.color
+local draw = OSGL.draw
 
-local myWindow = Window.new(windowUi, { sizeX = 50, sizeY = 50 })
-myWindow:Clear(color.BLACK)
-myWindow:Render()
+local SIZE_X = 1024
+local SIZE_Y = 1024
+
+local myWindow = Window.new(windowUi, { sizeX = SIZE_X, sizeY = SIZE_Y })
+
+local iterationCount = 0
+local times = {}
+while iterationCount <= 10 do
+    local start = os.clock()
+
+    for x = 0, SIZE_X - 1 do
+        for y = 0, SIZE_Y - 1 do
+            draw.pixel(myWindow, x, y, color.new(math.random(0, 255), math.random(0, 255), math.random(0, 255)))
+        end
+    end
+
+    myWindow:Render()
+
+    table.insert(times, os.clock() - start)
+    iterationCount += 1
+
+    task.wait()
+end
+
+print(times)
