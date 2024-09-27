b2035a13e477af2663952513eae40e35c2326acd
Gunshot Sound Studios <zeindigorenderer@gmail.com>
2024-09-27 07:09:31 +0100

    fixed DOCs

--- tests/init.client.lua
+++ tests/init.client.lua
@@ -1,38 +1,25 @@
---!optimize 2
--- ^^ This flag optimises
+-- Script
 
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local UserInputService = game:GetService("UserInputService")
+local finished = false
+
+UserInputService.WindowFocusReleased:Connect(function()
+	if not finished then
+		print("WHY WINDOW RELEASED? THIS WILL MAKE IT INACCURATE")
+	end
+end)
+
 local Players = game:GetService("Players")
-
-local windowUi = Players.LocalPlayer.PlayerGui:WaitForChild("ScreenGui").Window
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local OSGL = require(ReplicatedStorage.OSGL)
-local Window = OSGL.Window
+local window = OSGL.Window
 local color = OSGL.color
 local draw = OSGL.draw
 
-local SIZE_X = 1024
-local SIZE_Y = 1024
+local myWindow = window.new(Players.LocalPlayer.PlayerGui:WaitForChild("ScreenGui").Window, { sizeX = 1024, sizeY = 1024 })
 
-local myWindow = Window.new(windowUi, { sizeX = SIZE_X, sizeY = SIZE_Y })
+draw.circle(myWindow, { centerX = 0, centerY = 0, radius = 500, fillColor = color.RED })
+draw.tint(myWindow, color.GREEN, .1)
 
-local iterationCount = 0
-local times = {}
-while iterationCount <= 10 do
-    local start = os.clock()
-
-    for x = 0, SIZE_X - 1 do
-        for y = 0, SIZE_Y - 1 do
-            draw.pixel(myWindow, x, y, color.new(math.random(0, 255), math.random(0, 255), math.random(0, 255)))
-        end
-    end
-
-    myWindow:Render()
-
-    table.insert(times, os.clock() - start)
-    iterationCount += 1
-
-    task.wait()
-end
-
-print(times)
+myWindow:Render()
